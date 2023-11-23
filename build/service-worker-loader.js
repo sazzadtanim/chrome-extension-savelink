a8d0c9922f56f1b95da38b63965267b425b2e685
Sazzad Hossain <tanimsazzad@gmail.com>
2023-11-23 22:57:23 +0600

    init with build

--- build/service-worker-loader.js
+++ build/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5173/@vite/env'
-import 'http://localhost:5173/@crx/client-worker'
-import 'http://localhost:5173/src/background/index.ts'
+import './assets/chunk-208b7617.js';
