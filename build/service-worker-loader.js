f78a850ef02970ef12e546b3de02917e7c850a12
Sazzad Hossain <tanimsazzad@gmail.com>
2023-11-24 11:28:29 +0600

    update version number to 2

--- build/service-worker-loader.js
+++ build/service-worker-loader.js
@@ -1,1 +1,3 @@
-import './assets/chunk-ed2c53cf.js';
+import 'http://localhost:5173/@vite/env';
+import 'http://localhost:5173/@crx/client-worker';
+import 'http://localhost:5173/src/background/index.ts';
